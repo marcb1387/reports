a35230e643fd59f322f9e3a53a3dd93f653a7120
marcb1387 <marcb1387@gmail.com>
2019-12-09 14:41:52 -0500

    Update report.sh

--- report.sh
+++ report.sh
@@ -2,7 +2,7 @@
 now=$(date +"%m_%d_%Y")
 yesterday=$(date -d "yesterday" '+%m_%d_%Y')
 T1=$(mysql -u  -p -D pogo -se "select count(*) from trs_quest inner join custom_pokestop_area on trs_quest.GUID = custom_pokestop_area.pokestop_id and DATE(FROM_UNIXTIME(quest_timestamp)) = CURDATE() and custom_pokestop_area.area = 'dundalk'")
-T2=$(mysql -u  -p -D pogo -se  "SELECT `routefile`, (CHAR_LENGTH(routefile) - CHAR_LENGTH(REPLACE(routefile, '"', '')))DIV 2 FROM `pogo`.`settings_routecalc` WHERE routecalc_id ='1';")
+T2=$(mysql -u  -p -D pogo -se  "SELECT `routefile`, (CHAR_LENGTH(routefile) - CHAR_LENGTH(REPLACE(routefile, '"', '')))DIV 2 FROM `pogo`.`settings_routecalc` WHERE routecalc_id ='12';")
             if [ "$T1" = "$T2" ]; then
                 echo value is true
                 if [ ! -f /home/marc/report/temp/dundalk_$now.temp ]; then
