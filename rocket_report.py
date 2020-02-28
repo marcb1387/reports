dfaf94dbcaac479b921a02346ff99b0a4c304a34
marcb1387 <marcb1387@gmail.com>
2020-02-28 08:54:23 -0500

    Update rocket_report.py
    add port variable

--- rocket_report.py
+++ rocket_report.py
@@ -17,6 +17,7 @@ area = config.get('CONFIG', 'Area')
 author = config.get('CONFIG', 'Author')
 host = config.get('DATABASE', 'MAD_db_host')
 database = config.get('DATABASE', 'db_name')
+port = config.get('DATABASE', 'port')
 user = config.get('DATABASE', 'db_user')
 passwd = config.get('DATABASE', 'db_pass')
 # CONFIG END
@@ -24,7 +25,7 @@ passwd = config.get('DATABASE', 'db_pass')
 
 #Pokemon - Standard Task
 def rocket(leader,lname,sprite,guide):
- mariadb_connection = mariadb.connect(user=user, password=passwd, database=database, host=host)
+ mariadb_connection = mariadb.connect(user=user, password=passwd, database=database, host=host, port=port)
  cursor = mariadb_connection.cursor()
  query = ("select CONVERT(pokestop.name USING UTF8MB4) as pokestopname,pokestop.latitude,pokestop.longitude from pokestop WHERE incident_grunt_type = "+leader+" and ST_CONTAINS(ST_GEOMFROMTEXT('POLYGON(("+area+"))'), point(pokestop.latitude, pokestop.longitude))")
  cursor.execute(query)
