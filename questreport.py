285ee2b45474064c8f9bd912da7694cad527fcdf
marcb1387 <marcb1387@gmail.com>
2020-06-01 19:56:06 -0400

    update spinda number

--- questreport.py
+++ questreport.py
@@ -620,7 +620,7 @@ def stuff():
         quest_mon('315', 'Roselia', ':sparkles:', '', '00')
         quest_mon('317', 'Swalot', '', '', '00')
         quest_mon('325', 'Spoink', ':sparkles:', '', '00')
-        quest_mon('327', 'Spinda Number 7', ':sparkles:', '', '00')
+        quest_mon('327', 'Spinda Number 5', ':sparkles:', '', '00')
         quest_mon('331', 'Cacnea', '', '', '00')
         quest_mon('335', 'Zangoose', ':sparkles:', '', '00')
         quest_mon('336', 'Seviper', ':sparkles:', '', '00')
